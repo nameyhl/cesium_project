9e04ee1daa9bdbd813fa0327d388a692a868ee2b
youhelin <youhelin@51creation.com>
2024-11-22 16:08:44 +0800

    indexDB未完成

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,21 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
 
     {
-        name:'home',
-        path:'/',
-        component: ()=>import('@/views/home/index.vue'),
-        children:[{
-            name:'earth',
-            path:'/earth',
-            component: ()=>import('@/views/earth/index.vue')
+        name: 'home',
+        path: '/',
+        component: () => import('@/views/home/index.vue'),
+        children: [{
+            name: 'earth',
+            path: '/earth',
+            component: () => import('@/views/earth/index.vue')
         }
-    ]
+        ]
     },
+    {
+        name: 'login',
+        path: '/login',
+        component: () => import('@/views/login/index.vue')
+    }
 ]
 
 const router = createRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import indexDB from '@/indexDb/index.js'
+console.log(indexDB);
+
+const stratDB = async () => {
+    const dbName = 'myDB', storeName = 'userDB'
+    const db = await indexDB.openDB(dbName, storeName, 1)
+    let data = await indexDB.addData(db, storeName, {
+        id : 111,
+        name : '张三',
+        userName : 'zhangsan',
+        psw : '123456',
+        desc: '张三的账号' 
+    })
+    console.log(data);
+    // let data = await indexDB.getDataById(db, storeName, 111)
+    // console.log(data);
+    // let data = await indexDB.updateDB(db, storeName, {
+    //     id : 111,
+    //     desc: "修改了desc"
+    // })
+    // console.log(data);
+
+    // let data = await indexDB.deleteData(db, storeName, 111)
+    // console.log(data);
+}
+
+</script>
+<template>
+</template>
+<style scoped>
+</style>
